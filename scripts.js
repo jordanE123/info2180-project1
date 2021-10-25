a773b12f4ed9e27f50ac9997bb143764924a9b57
Jordan Earle <earle_knight@hotmail.com>
2021-10-24 22:09:33 -0500

    Final product

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-/* Add your JavaScript to this file */
+window.onload = function(){
+
+    var myForm = document.querySelector("div form");
+    var theMessage = document.querySelector(".message");
+
+
+    myForm.addEventListener('submit', e =>{
+        e.preventDefault();
+
+        var email = document.getElementById("email").value;
+
+        if(email != ""){
+            theMessage.innerHTML = `Thank you! Your email address ${email} has been added to our mailing list!`;
+        }else{
+            theMessage.innerHTML = 'Please enter a valid email address';
+
+        }
+
+    })
+}
